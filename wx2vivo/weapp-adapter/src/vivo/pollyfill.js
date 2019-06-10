05199c8485a910d005ea45c996ee0bb5e8943827
yegao <familyink@163.com>
2019-06-10 17:16:40 +0800

    canvas

--- wx2vivo/weapp-adapter/src/vivo/pollyfill.js
+++ wx2vivo/weapp-adapter/src/vivo/pollyfill.js
@@ -1,3 +1,205 @@
+var gl = __gl;
+//顶点着色器源码
+var vertexShader_source = "attribute vec4 a_Position;\n" +
+    "attribute vec2 a_TextureCoord;\n" +
+    "varying vec2 v_TextureCoord;\n" +
+    "void main()\n" +
+    "{\n" +
+    "   gl_Position = a_Position;\n" +
+    "   v_TextureCoord = a_TextureCoord;\n" +
+    "}\n";
+
+//片元着色器源码
+var fragmentShader_source = "precision lowp float;\n" +
+    "uniform sampler2D u_Sampler;\n" +
+    "varying vec2 v_TextureCoord;\n" +
+    "void main() {\n" +
+    "   gl_FragColor = texture2D(u_Sampler, v_TextureCoord);\n" +
+    "}\n";
+
+function loadShader(gl, shaderType, shaderSource) {
+    var shader = gl.createShader(shaderType);
+    gl.shaderSource(shader, shaderSource);
+    gl.compileShader(shader);
+    //返回是否链接成功
+    var compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (!compiled) {
+        console.log("shader not compiled!");
+        console.log(gl.getShaderInfoLog(shader));
+    }
+    return shader;
+}
+
+var positionLocation, textureCoordLocation;
+function createShaderProgram(gl, vertexShader, fragmentShader) {
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    var compiled = gl.getProgramParameter(shaderProgram, gl.LINK_STATUS);
+    if (!compiled) {
+        console.log("program not compiled");
+        console.log(gl.getProgramInfoLog(shaderProgram));
+        return;
+    }
+    positionLocation = gl.getAttribLocation(shaderProgram, "a_Position");
+    textureCoordLocation = gl.getAttribLocation(shaderProgram, "a_TextureCoord");
+    return shaderProgram;
+}
+
+var vertexShader, fragmentShader, shaderProgramcanvas, vertexBuffer, indexBuffer, canvasTexture;
+const initTexCanvas = function (canvas) {
+    gl = canvas.getContext("webgl");
+
+    vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShader_source);
+    fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragmentShader_source);
+    shaderProgramcanvas = createShaderProgram(gl, vertexShader, fragmentShader);
+
+    vertexBuffer = gl.createBuffer();
+    indexBuffer = gl.createBuffer();
+    canvasTexture = gl.createTexture();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+    // setTextureCanvas(canvas);
+}
+
+const setTextureCanvas = function (canvas) {
+    //texture
+    // canvasTexture = gl.createTexture();
+
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+    gl.bindTexture(gl.TEXTURE_2D, canvasTexture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    //  console.log("xxxxxx " + window.__canvas._alignment);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, canvas);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+}
+
+function getarrayVertexAttrib(gl, location) {
+    var binding = gl.getVertexAttrib(location, gl.VERTEX_ATTRIB_ARRAY_BUFFER_BINDING);
+    var enabled = gl.getVertexAttrib(location, gl.VERTEX_ATTRIB_ARRAY_ENABLED);
+    var size = gl.getVertexAttrib(location, gl.VERTEX_ATTRIB_ARRAY_SIZE);
+    var type = gl.getVertexAttrib(location, gl.VERTEX_ATTRIB_ARRAY_TYPE);
+    var normalized = gl.getVertexAttrib(location, gl.VERTEX_ATTRIB_ARRAY_NORMALIZED);
+    var stride = gl.getVertexAttrib(location, gl.VERTEX_ATTRIB_ARRAY_STRIDE);
+    var offset = gl.getVertexAttribOffset(location, gl.VERTEX_ATTRIB_ARRAY_POINTER);
+    var data = {
+        "binding": binding,
+        "enabled": enabled,
+        "size": size,
+        "type": type,
+        "normalized": normalized,
+        "stride": stride,
+        "offset": offset,
+    }
+    return data;
+}
+
+//顶点数组
+var vertices = new Float32Array([
+    //0
+    -1.0, 1.0,
+    0, 0,
+    //1
+    1.0, 1.0,
+    1, 0,
+    //2
+    1.0, -1.0,
+    1, 1,
+    //3
+    -1.0, -1.0,
+    0, 1,
+]);
+//索引数组
+var indices = new Uint16Array([0, 1, 2, 0, 2, 3]);
+const renderTexCanvas = function () {
+    //record previous render data
+    var shaderIndex = gl.getParameter(gl.CURRENT_PROGRAM);
+    var arrayBuffer = gl.getParameter(gl.ARRAY_BUFFER_BINDING);
+    var elementBuffer = gl.getParameter(gl.ELEMENT_ARRAY_BUFFER_BINDING);
+    var texture2D = gl.getParameter(gl.TEXTURE_BINDING_2D);
+    var positionData = getarrayVertexAttrib(gl, positionLocation);
+    var textureCoordData = getarrayVertexAttrib(gl, textureCoordLocation);
+
+    //canvas render data
+    gl.useProgram(shaderProgramcanvas);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 4 * 4, 0);
+    gl.enableVertexAttribArray(positionLocation);
+
+    gl.vertexAttribPointer(textureCoordLocation, 2, gl.FLOAT, false, 4 * 4, 2 * 4);
+    gl.enableVertexAttribArray(textureCoordLocation);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, canvasTexture);
+
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+
+    //recover previous render data
+    gl.useProgram(shaderIndex);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionData["binding"]);
+    gl.enableVertexAttribArray(positionData["enabled"]);
+    gl.vertexAttribPointer(positionLocation, positionData["size"], positionData["type"],
+        positionData["normalized"], positionData["stride"], positionData["offset"]);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordData["binding"]);
+    gl.enableVertexAttribArray(textureCoordData["enabled"]);
+    gl.vertexAttribPointer(textureCoordLocation, textureCoordData["size"], textureCoordData["type"],
+        textureCoordData["normalized"], textureCoordData["stride"], textureCoordData["offset"]);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, arrayBuffer);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elementBuffer);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture2D);
+}
+
+
+
+window.canvas = window.mainCanvas || qg.createCanvas()
+canvas.__proto__.__proto__ = new HTMLElement('canvas')
+const ctx = canvas.getContext('2d')
+const oldGetContext = canvas.getContext;
+canvas.getContext = function(arg){
+  if(arg === '2d') {
+    return ctx
+  }
+  else{
+    return oldGetContext(arg)
+  }
+}
+
+if(!oldDrawImage){
+  if(oldDrawImage = ctx.drawImage){
+    ctx.constructor.prototype.drawImage = function(...args){
+      oldDrawImage.call(ctx,...args)
+      setTextureCanvas(canvas);
+      // renderTexCanvas();
+    }
+  }
+}
+/** vivo end */
+
+canvas.getBoundingClientRect = () => {
+  return {
+    top: 0,
+    left: 0,
+    width: window.innerWidth,
+    height: window.innerHeight
+  }
+}
+initTexCanvas(canvas)
 window.wx = {}
 wx.getLaunchOptionsSync = noop
 const noop = function(){}
@@ -141,8 +343,14 @@ wx.showToast = function(data){
     if(data){
         qg.showToast({
             message: data.title || '',
-            duration: data.duration || 0
+            duration: (1500 < data.duration) && 1 || 0
         })
+        if(typeof data.success === 'function'){
+            data.success()
+        }
+        if(typeof data.complete === 'function'){
+            data.complete()
+        }
     }
 }
 wx.showModal = function(data){
@@ -188,7 +396,10 @@ wx.offKeyboardComplete = qg.offKeyboardComplete
 wx.hideKeyboard = function(data){
     qg.hideKeyboard();
     if(typeof data.success === 'function'){
-        data.success()
+        data.success({errMsg: "hideKeyboard:ok"})
+    }
+    if(typeof data.complete === 'function'){
+        data.complete({errMsg: "hideKeyboard:ok"})
     }
 }
 wx.setMenuStyle = noop
@@ -200,7 +411,15 @@ wx.offWindowResize = noop // 需要适配
 
 // 网络
 wx.request = qg.request
-wx.downloadFile = qg.download // 和微信有点区别 不好适配
+wx.downloadFile = function(data){  // 微信存在filePath可选参数指定文件下载后存储的路径，该适配不支持
+    return qg.download({
+        url: data.url,
+        header: data.header,
+        success: data.success || noop,
+        fail: data.fail || noop,
+        complete: data.complete || noop
+    })
+}
 wx.uploadFile = function(data){
     return qg.uploadFile({
         url:data.url,
@@ -208,9 +427,9 @@ wx.uploadFile = function(data){
         method:'POST',
         data: data.formData,
         header: data.header,
-        success: data.success,
-        fail: data.fail,
-        complete: data.complete
+        success: data.success || noop,
+        fail: data.fail || noop,
+        complete: data.complete || noop
     })
 }
 
@@ -617,27 +836,65 @@ wx.getFileSystemManager = qg.getFileSystemManager = function(){
 
 // 开放接口
 wx.navigateToMiniProgram = noop
+// qg.authorize({
+//     type: "token",
+//     success: function (obj) {
+//         qg.showToast({
+//             message: "handling success, code: " + obj.accessToken
+//         })
+//         qg.getProfile({
+//             token: obj.accessToken,
+//             success: function(data){
+//                 qg.showToast({
+//                     message: "nickname: " + data.nickname
+//                 })
+//             },
+//             fail: function(data, code) {
+//                 qg.showToast({
+//                     message: "handling fail, code=" + code
+//                 })
+//             }
+//         })
+//     },
+//     fail: function (data, code) {
+//         qg.showToast({
+//             message: "handling fail, fail code: " + code
+//         })
+//     }
+// })
 wx.authorize = function(data){ // 不好适配
-    return qg.authorize({
-        type: data.type || "token",
-        redirectUri: data.redirectUri,
-        scope: data.scope,
-        state: data.state,
-        success: data.success || noop,
-        fail: data.fail || noop,
-        complete: data.complete || noop
+    qg.showToast({
+        message:"无法自动适配wx.authorize,请使用qg.authorize和qg.getProfile进行适配"
     })
+    console.log("无法自动适配wx.authorize,请使用qg.authorize和qg.getProfile进行适配,具体请参考https://minigame.vivo.com.cn/documents/#/api/service/account")
+    // return qg.authorize({
+    //     type: data.type || "token",
+    //     redirectUri: data.redirectUri,
+    //     scope: data.scope,
+    //     state: data.state,
+    //     success: data.success || noop,
+    //     fail: data.fail || noop,
+    //     complete: data.complete || noop
+    // })
 }
 wx.getUserInfo = function(data){ // 不好适配
-    // console.log("获取用户信息请手动适配")
-    return qg.getProfile({
-        token: data.token,
-        success: data.success || noop,
-        fail: data.fail || noop,
-        complete: data.complete || noop
+    qg.showToast({
+        message:"无法自动适配wx.getUserInfo,请使用qg.getProfile进行适配"
     })
+    console.log("无法自动适配wx.createUserInfoButton,请使用qg.authorize和qg.getProfile进行适配,具体请参考https://minigame.vivo.com.cn/documents/#/api/service/account")    // console.log("获取用户信息请手动适配")
+    // return qg.getProfile({
+    //     token: data.token,
+    //     success: data.success || noop,
+    //     fail: data.fail || noop,
+    //     complete: data.complete || noop
+    // })
 }
+
 wx.createUserInfoButton = function(){
+    qg.showToast({
+        message:"无法自动适配wx.createUserInfoButton,请使用qg.authorize和qg.getProfile进行适配"
+    })
+    console.log("无法自动适配wx.createUserInfoButton,请使用qg.authorize和qg.getProfile进行适配,具体请参考https://minigame.vivo.com.cn/documents/#/api/service/account")
     return {
         destroy(){},
         hide(){},
